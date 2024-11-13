0418be6219fdb99ac653ed82a2e30032996eb62c
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2024-11-13 01:40:25 -0400

    apuntes sección 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/counter.module';
-
-import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -18,6 +18,7 @@ import { HeroesModule } from './heroes/heroes.module';
     AppRoutingModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
